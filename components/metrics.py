ec0b9188716c3fab1820a138d4c9e497b223ea33
mayankdusad87-ai <mayankdusad87@gmail.com>
2026-07-07 15:25:41 +0530

    Update metrics.py

--- components/metrics.py
+++ components/metrics.py
@@ -1,1 +1,24 @@
+"""
+Metric Cards
+"""
 
+import streamlit as st
+
+
+def metric_card(
+    title,
+    value,
+    delta=None,
+    delta_color="normal",
+):
+    """
+    Standard KPI card.
+    """
+
+    st.metric(
+        label=title,
+        value=value,
+        delta=delta,
+        delta_color=delta_color,
+        border=True,
+    )
